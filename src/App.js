ab201de99ec0b148de0bd1794a213baa7e8a8fd3
Brian Illes <brianrilles@gmail.com>
2019-06-24 19:11:39 -0700

    Establishes routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import Blog from "./components/Blog.js";
+import Projects from "./components/Projects.js";
+import About from "./components/About.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">hello world</header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="nav">
+          <Navbar />
+        </div>
+        <div className="body">
+          <Route exact path="/" component={Projects} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/about" component={About} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
