15810a22a148ba253238ee848ecd81a6c2a17ccf
Brian Illes <brianrilles@gmail.com>
2019-06-24 21:40:08 -0700

    Adds Github, linkedin, and twitter links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,71 @@
 import React from "react";
 import "../styles/Navbar.scss";
+import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+import { Github } from "styled-icons/boxicons-logos/Github";
+import { Linkedin } from "styled-icons/boxicons-logos/Linkedin";
+import { Twitter } from "styled-icons/boxicons-logos/Twitter";
+
+const GithubDark = styled(Github)`
+  color: #000;
+  height: 20x;
+  width: 20px;
+  opacity: 0.6;
+  :hover {
+    opacity: 1;
+  }
+`;
+
+const LinkedinDark = styled(Linkedin)`
+  color: #000;
+  height: 20px;
+  width: 20px;
+  opacity: 0.6;
+  :hover {
+    opacity: 1;
+  }
+`;
+
+const TwitterDark = styled(Twitter)`
+  color: #000;
+  height: 20px;
+  width: 20px;
+  opacity: 0.6;
+  :hover {
+    opacity: 1;
+  }
+`;
 
 export default function Navbar() {
   return (
     <div className="Navbar">
-      <NavLink exact to="/">
-        <h1>Brian Illes</h1>
-      </NavLink>
-      <div className="nav-right">
-        <NavLink exact to="/" activeClassName="activeNavLink">
-          Projects
+      <div className="top">
+        <NavLink exact to="/">
+          <h1 className="name">Brian Illes</h1>
         </NavLink>
-        <NavLink to="/blog" activeClassName="activeNavLink">
-          Blog
-        </NavLink>
-        <NavLink to="/about" activeClassName="activeNavLink">
-          About
-        </NavLink>
+        <div className="nav-right">
+          <NavLink exact to="/" activeClassName="activeNavLink">
+            Projects
+          </NavLink>
+          <NavLink to="/blog" activeClassName="activeNavLink">
+            Blog
+          </NavLink>
+          <NavLink to="/about" activeClassName="activeNavLink">
+            About
+          </NavLink>
+        </div>
+      </div>
+      <div className="bottom">
+        <a href="https://github.com/brilles">
+          <GithubDark />
+        </a>
+        <a href="https://www.linkedin.com/in/brian-illes/">
+          <LinkedinDark />
+        </a>
+        <a href="https://twitter.com/brianilles">
+          <TwitterDark />
+        </a>
+        <span> | brianrilles@gmail.com</span>
       </div>
     </div>
   );
