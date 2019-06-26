296b704ccd2db10fb8ca6406e4f002df7b48e56e
Brian Illes <brianrilles@gmail.com>
2019-06-25 18:58:31 -0700

    clean

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from "react";
-import "./scss/App.scss";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/Home.js";
-import Projects from "./components/Projects.js";
-import About from "./components/About.js";
-import Contact from "./components/Contact.js";
-import Navbar from "./components/Navbar.js";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <div className="navbar-wrapper">
-          <Navbar />
-        </div>
-        <div className="main-wrapper">
-          <Route exact path="/" component={Home} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      <p>coming soon</p>
+    </div>
   );
 }
 
