b48f4243428c7718bca0c29af8dcb19279bd9be9
Brian Illes <brianrilles@gmail.com>
2019-07-08 20:52:46 -0700

    2.0 - clean, favicon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import About from "./components/About.js";
-import Home from "./components/Home.js";
-import Navbar from "./components/Navbar.js";
-import "./scss/App.scss";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <div className="navbar">
-          <Navbar />
-        </div>
-        <div className="main">
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <p>Sanity</p>
+      </header>
+    </div>
   );
 }
 
