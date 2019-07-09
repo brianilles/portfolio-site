9c004afb4a63931740ec0e5b3ec60fbad412fc43
Brian Illes <brianrilles@gmail.com>
2019-07-08 21:47:30 -0700

    adds footer and routes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.js";
 import "./scss/App.scss";
+import Footer from "./components/Footer.js";
+import Home from "./components/Home.js";
+import About from "./components/About.js";
+import Projects from "./components/Projects.js";
+import Contact from "./components/Contact.js";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <div className="nav-wrapper">
-            <Navbar />
-          </div>
-          <div className="main-wrapper">Main</div>
-          <div className="footer-wrapper">Footer</div>
-        </header>
+        <div className="nav-wrapper">
+          <Navbar />
+        </div>
+        <div className="main-wrapper">
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </div>
+        <div className="footer-wrapper">
+          <Footer />
+        </div>
       </div>
     </Router>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../scss/Footer.scss";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="left">
+        <p>icons</p>
+      </div>
+      <div className="right">© 2019 Brian Illes</div>
+    </div>
+  );
+}
