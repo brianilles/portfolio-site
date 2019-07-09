09fb73f3b62ea363fa2d6106ccbfb7fbb83f9ea6
Brian Illes <brianrilles@gmail.com>
2019-07-09 07:41:30 -0700

    Footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,41 @@
 import React from "react";
 import "../scss/Footer.scss";
+import styled from "styled-components";
+import { Github } from "styled-icons/boxicons-logos/Github";
+import { Twitter } from "styled-icons/boxicons-logos/Twitter";
+import { LinkedinSquare } from "styled-icons/boxicons-logos/LinkedinSquare";
+
+const GithubLight = styled(Github)`
+  color: #fff;
+  height: 25px;
+  width: 25px;
+`;
+
+const TwitterLight = styled(Twitter)`
+  color: #fff;
+  height: 25px;
+  width: 25px;
+`;
+
+const LinkedinLight = styled(LinkedinSquare)`
+  color: #fff;
+  height: 25px;
+  width: 25px;
+`;
 
 export default function Footer() {
   return (
     <div className="footer">
       <div className="left">
-        <p>icons</p>
+        <a href="https://github.com/brilles">
+          <GithubLight />
+        </a>
+        <a href="https://www.linkedin.com/in/brian-illes/">
+          <LinkedinLight />
+        </a>
+        <a href="https://twitter.com/brianilles">
+          <TwitterLight />
+        </a>
       </div>
       <div className="right">© 2019 Brian Illes</div>
     </div>
