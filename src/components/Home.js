7436d42f1b0eaa277e4ce0fb58aa11de35eca8a3
Brian Illes <brianrilles@gmail.com>
2019-07-08 22:00:36 -0700

    main

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Tabs from "./Tabs.js";
+import "../scss/Home.scss";
 
 export default function Home() {
   return (
     <div>
-      <p>Home</p>
+      <div className="top">
+        <h1>Brian Illes</h1>
+        <h2>Full-Stack Software Developer</h2>
+      </div>
+      <div className="bottom">
+        <Tabs />
+      </div>
     </div>
   );
 }
